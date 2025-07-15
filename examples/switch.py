46ee2ca4944e670a51a5b41e72ef768d78a14556
Jonas da Silva <phoenixr41414141@gmail.com>
2025-07-16 01:58:40 +0200

    📝 Extend switch example

--- examples/switch.py
+++ examples/switch.py
@@ -4,11 +4,15 @@ from loguru import logger
 from rich import print
 
 from powercli import Command
+from powercli.typedefs import Context
 from powercli.methods import Switch
 from powercli.utils import static
 
 logger.enable("powercli.command")
 logger.add(sys.stdout, level="TRACE")
+
+def disable_color(_: Context[str, None]) -> None:
+    print("Disabling color...")
 
 cmd: Command[str, None] = Command()
 cmd.flag(
@@ -16,7 +20,13 @@ cmd.flag(
     short="f",
     method=Switch(on_presence=static("A"), on_absence=static("B")),
 )
+cmd.flag(
+    identifier="no-color",
+    long="no-color",
+    method=Switch(on_presence=disable_color, on_absence=static(None))
+)
 
 if __name__ == "__main__":
     args = cmd.parse_args()
     print(args.value_of("f"))
+    print(args.value_of("no-color"))
