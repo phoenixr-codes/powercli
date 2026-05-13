8bb6044fd3f9033fa19ad6bef096181082018b88
Jonas da Silva <jonasdasilva5@proton.me>
2026-05-13 22:58:59 +0200

    🎨 Format code

--- examples/compiler.py
+++ examples/compiler.py
@@ -1,7 +1,7 @@
 import sys
 from typing import Any
 
-from adorable.common import AQUA, GREEN, YELLOW, PURPLE
+from adorable.common import AQUA, GREEN, PURPLE, YELLOW
 from loguru import logger
 from rich import print
 
